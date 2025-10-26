7569caf68d1e0ccf197afce3d0b2f7482de3fe25
Caotinification <nguyenjustin930@gmail.com>
2025-10-26 17:02:35 -0400

    commented out kickstart defaults. plugins now loaded automatically

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -11,8 +11,6 @@
 -- NOTE: Here is where you install your plugins.
 require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
-  'NMAC427/guess-indent.nvim', -- Detect tabstop and shiftwidth automatically
-
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
   -- keys can be used to configure plugin behavior/loading/etc.
@@ -23,25 +21,16 @@ require('lazy').setup({
   -- modular approach: using `require 'path.name'` will
   -- include a plugin definition from file lua/path/name.lua
 
-  require 'kickstart.plugins.gitsigns',
-
-  require 'kickstart.plugins.which-key',
-
-  require 'kickstart.plugins.telescope',
-
-  require 'kickstart.plugins.lspconfig',
-
-  require 'kickstart.plugins.conform',
-
-  require 'kickstart.plugins.blink-cmp',
-
-  require 'kickstart.plugins.tokyonight',
-
-  require 'kickstart.plugins.todo-comments',
-
-  require 'kickstart.plugins.mini',
-
-  require 'kickstart.plugins.treesitter',
+  --  require 'kickstart.plugins.gitsigns',
+  --  require 'kickstart.plugins.which-key',
+  --  require 'kickstart.plugins.telescope',
+  --  require 'kickstart.plugins.lspconfig',
+  --  require 'kickstart.plugins.conform',
+  --  require 'kickstart.plugins.blink-cmp',
+  --  require 'kickstart.plugins.tokyonight',
+  --  require 'kickstart.plugins.todo-comments',
+  --  require 'kickstart.plugins.mini',
+  --  require 'kickstart.plugins.treesitter',
 
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
@@ -62,7 +51,7 @@ require('lazy').setup({
   --    This is the easiest way to modularize your config.
   --
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
-  -- { import = 'custom.plugins' },
+  { import = 'plugins' },
   --
   -- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
   -- Or use telescope!
