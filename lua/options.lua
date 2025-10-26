521c288f15f5edfc3b1e13afb0869116e606300d
Caotinification <nguyenjustin930@gmail.com>
2025-10-26 05:02:11 -0400

    added my own options

--- lua/options.lua
+++ lua/options.lua
@@ -7,11 +7,11 @@
 vim.o.number = true
 -- You can also add relative line numbers, to help with jumping.
 --  Experiment for yourself to see if you like it!
--- vim.o.relativenumber = true
-
+vim.o.relativenumber = true
+vim.o.termguicolors = true
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.o.mouse = 'a'
-
+vim.o.linebreak = true
 -- Don't show the mode, since it's already in the status line
 vim.o.showmode = false
 
@@ -71,4 +71,23 @@ vim.o.scrolloff = 10
 -- See `:help 'confirm'`
 vim.o.confirm = true
 
+-- Save folds upon close.
+vim.api.nvim_create_autocmd('BufWinLeave', {
+  pattern = '*.*',
+  callback = function()
+    vim.cmd 'mkview'
+  end,
+})
+-- Load them
+vim.api.nvim_create_autocmd('BufWinEnter', {
+  pattern = '*.*',
+  callback = function()
+    vim.cmd 'silent! loadview'
+  end,
+})
+
+-- Neovide specific settings
+if vim.g.neovide then
+  vim.g.neovide_cursor_vfx_mode = { 'torpedo', 'ripple' }
+end
 -- vim: ts=2 sts=2 sw=2 et
