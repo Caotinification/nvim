90ceed8aa0e1a8324d73aa232930439d189229d5
Caotinification <nguyenjustin930@gmail.com>
2025-10-27 19:04:29 -0400

    changed j,k to gj, gk when tex buffers are opened

--- after/ftplugin/tex.lua
+++ after/ftplugin/tex.lua
@@ -2,3 +2,5 @@
 vim.bo.tabstop = 2 -- 'ts': Number of spaces a tab visually occupies
 vim.bo.shiftwidth = 2 -- 'sw': Number of spaces used for indentation (< and >)
 vim.bo.softtabstop = 2 -- 'sts': Makes the <Tab> key insert spaces up to this width
+vim.keymap.set('n', 'k', 'gk', { buffer = true, desc = 'Move up a display line' })
+vim.keymap.set('n', 'j', 'gj', { buffer = true, desc = 'Move down a display line' })
