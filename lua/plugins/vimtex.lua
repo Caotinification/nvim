b176126e95c0c15dec24ce85ce919b2b642c054f
Caotinification <nguyenjustin930@gmail.com>
2025-10-27 17:01:49 -0400

    readded old config

--- lua/plugins/vimtex.lua
+++ lua/plugins/vimtex.lua
@@ -4,6 +4,26 @@ return {
   -- tag = "v2.15", -- uncomment to pin to a specific release
   init = function()
     -- VimTeX configuration goes here, e.g.
+    vim.g.loaded_netrw = true
+    vim.g.loaded_netrwPlugin = true
+    vim.g.vimtex_complete_enabled = true
+    vim.g.vimtex_complete_close_braces = true
     vim.g.vimtex_view_method = 'sioyek'
+    vim.g.vimtex_compiler_method = 'latexmk'
+    vim.g.vimtex_compiler_latexmk = {
+      options = {
+        '-xelatex',
+        '-verbose',
+        '-file-line-error',
+        '-synctex=1',
+        '-interaction=nonstopmode',
+      },
+    }
+    vim.g.vimtex_quickfix_ignore_filters = {
+      'You have requested package',
+      'Underfull',
+      'Overfull',
+    }
+    vim.g.vimtex_quickfix_open_on_warning = 0
   end,
 }
