5e55a5cf2d42fc1a3da684ab4bf805fd42d97681
Caotinification <nguyenjustin930@gmail.com>
2025-10-29 19:56:35 -0400

    ocaml ft config to use 2 spaces

---
+++ after/ftplugin/ocaml.lua
@@ -0,0 +1,4 @@
+-- Sets options for the current buffer only (buffer-local)
+--vim.bo.tabstop = 2 -- 'ts': Number of spaces a tab visually occupies
+vim.bo.shiftwidth = 2 -- 'sw': Number of spaces used for indentation (< and >)
+--vim.bo.softtabstop = 2 -- 'sts': Makes the <Tab> key insert spaces up to this width
