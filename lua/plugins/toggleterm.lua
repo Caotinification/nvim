709d42994fa2e66f6c5f86364bc61a8d2a558bd2
Caotinification <nguyenjustin930@gmail.com>
2025-10-29 19:55:48 -0400

    toggleterm plugin

---
+++ lua/plugins/toggleterm.lua
@@ -0,0 +1,11 @@
+return {
+  'akinsho/toggleterm.nvim',
+  version = '*',
+  opts = {
+    size = 8,
+    hide_numbers = false,
+    open_mapping = '<leader>tt',
+    insert_mappings = false,
+    terminal_mappings = false, -- whether or not the open mapping applies in the opened terminals
+  },
+}
