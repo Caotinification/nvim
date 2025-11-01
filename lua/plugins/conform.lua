db8c41ac9304102798d9ddfe50c428035202c2e4
Caotinification <nguyenjustin930@gmail.com>
2025-11-01 17:27:02 -0400

    stylua doesnt remove parentheses anymore

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,50 +1,54 @@
 return {
-  { -- Autoformat
-    'stevearc/conform.nvim',
-    event = { 'BufWritePre' },
-    cmd = { 'ConformInfo' },
-    keys = {
-      --{
-      --  '<leader>f',
-      --  function()
-      --    require('conform').format { async = true, lsp_format = 'fallback' }
-      --  end,
-      --  mode = '',
-      --  desc = '[F]ormat buffer',
-      --},
-    },
-    opts = {
-      log_level = vim.log.levels.ERROR,
-      notify_on_error = true,
-      format_after_save = function(bufnr)
-        -- Disable "format_on_save lsp_fallback" for languages that don't
-        -- have a well standardized coding style. You can add additional
-        -- languages here or re-enable it for the disabled ones.
-        local disable_filetypes = { c = true, cpp = true }
-        if disable_filetypes[vim.bo[bufnr].filetype] then
-          return nil
-        else
-          return {
-            lsp_format = 'never',
-          }
-        end
-      end,
-      formatters = {
-        ocamlfmt = {
-          command = 'ocamlformat',
-          args = { '$FILENAME', '--margin', '139' },
-        },
-      },
-      formatters_by_ft = {
-        lua = { 'stylua' },
-        ocaml = { 'ocamlfmt' },
-        -- Conform can also run multiple formatters sequentially
-        -- python = { "isort", "black" },
-        --
-        -- You can use 'stop_after_first' to run the first available formatter from the list
-        -- javascript = { "prettierd", "prettier", stop_after_first = true },
-      },
-    },
-  },
+	{ -- Autoformat
+		"stevearc/conform.nvim",
+		event = { "BufWritePre" },
+		cmd = { "ConformInfo" },
+		keys = {
+			--{
+			--  '<leader>f',
+			--  function()
+			--    require('conform').format { async = true, lsp_format = 'fallback' }
+			--  end,
+			--  mode = '',
+			--  desc = '[F]ormat buffer',
+			--},
+		},
+		opts = {
+			log_level = vim.log.levels.ERROR,
+			notify_on_error = true,
+			format_after_save = function(bufnr)
+				-- Disable "format_on_save lsp_fallback" for languages that don't
+				-- have a well standardized coding style. You can add additional
+				-- languages here or re-enable it for the disabled ones.
+				local disable_filetypes = { c = true, cpp = true }
+				if disable_filetypes[vim.bo[bufnr].filetype] then
+					return nil
+				else
+					return {
+						lsp_format = "never",
+					}
+				end
+			end,
+			formatters = {
+				ocamlfmt = {
+					command = "ocamlformat",
+					args = { "$FILENAME", "--margin", "139" },
+				},
+				luafmt = {
+					command = "stylua",
+					args = { "$FILENAME", "--call-parentheses", "Always" },
+				},
+			},
+			formatters_by_ft = {
+				lua = { "luafmt" },
+				ocaml = { "ocamlfmt" },
+				-- Conform can also run multiple formatters sequentially
+				-- python = { "isort", "black" },
+				--
+				-- You can use 'stop_after_first' to run the first available formatter from the list
+				-- javascript = { "prettierd", "prettier", stop_after_first = true },
+			},
+		},
+	},
 }
 -- vim: ts=2 sts=2 sw=2 et
