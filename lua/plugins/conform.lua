a544fb62d7a12499e9d1eb2720299b21e194266a
Caotinification <nguyenjustin930@gmail.com>
2025-11-01 14:15:23 -0400

    formats after save now

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -16,7 +16,7 @@ return {
     opts = {
       log_level = vim.log.levels.ERROR,
       notify_on_error = true,
-      format_on_save = function(bufnr)
+      format_after_save = function(bufnr)
         -- Disable "format_on_save lsp_fallback" for languages that don't
         -- have a well standardized coding style. You can add additional
         -- languages here or re-enable it for the disabled ones.
@@ -25,8 +25,7 @@ return {
           return nil
         else
           return {
-            timeout_ms = 500,
-            lsp_format = 'fallback',
+            lsp_format = 'never',
           }
         end
       end,
