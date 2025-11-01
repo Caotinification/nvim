33cfd6e01d55cdeff38bc534d10654f423650be2
Caotinification <nguyenjustin930@gmail.com>
2025-11-01 14:16:06 -0400

    switched to texlabs for lsp

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -219,8 +219,7 @@ return {
         --
         -- But for many setups, the LSP (`ts_ls`) will work just fine
         -- ts_ls = {},
-        --
-        ocamllsp = {},
+        ocamllsp = require 'lsp.ocamllsp',
         lua_ls = {
           -- cmd = { ... },
           -- filetypes = { ... },
@@ -253,7 +252,7 @@ return {
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
-        'ltex-ls-plus',
+        'texlab',
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
