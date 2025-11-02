0fa4733df1251b15500aa33b53c406d615b9327d
Caotinification <nguyenjustin930@gmail.com>
2025-11-02 01:30:21 -0500

    the misery

---
+++ lua/plugins/hardtime.lua
@@ -0,0 +1,11 @@
+return {
+  'm4xshen/hardtime.nvim',
+  lazy = false,
+  dependencies = { 'MunifTanjim/nui.nvim' },
+  opts = {
+    disabled_keys = {
+      ['<j>'] = false,
+      ['<k>'] = false,
+    },
+  },
+}
